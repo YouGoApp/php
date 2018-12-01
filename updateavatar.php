1999211478c1628d31041fa1a5d7e48b348e21a6
Hoa Dang <hoadv106@gmail.com>
2018-12-01 21:44:36 +0700

    update avatar

--- updateavatar.php
+++ updateavatar.php
@@ -7,46 +7,41 @@
 			$path = 'avatars/';
 			$extensions = ['jpg', 'jpeg', 'png', 'gif'];
 
-			$all_files = count($_FILES['avatar']['tmp_name']);
-echo $all_files;exit;
-			for ($i = 0; $i < $all_files; $i++) {  
-				$file_name = $_FILES['avatar']['name'][$i];
-				$file_tmp = $_FILES['avatar']['tmp_name'][$i];
-				$file_type = $_FILES['avatar']['type'][$i];
-				$file_size = $_FILES['avatar']['size'][$i];
-				$file_ext = strtolower(end(explode('.', $_FILES['avatar']['name'][$i])));
+			$file_name = $_FILES['avatar']['name'];
+			$file_tmp = $_FILES['avatar']['tmp_name'];
+			$file_type = $_FILES['avatar']['type'];
+			$file_size = $_FILES['avatar']['size'];
+			$fileParts = explode('.', $_FILES['avatar']['name']);
+			$file_ext = strtolower(end($fileParts));
+			$file = $path . $_POST['username'].'-'.$file_name;
 
-				$file = $path . $file_name;
+			if (!in_array($file_ext, $extensions)) {
+				$errors[] = 'Extension not allowed: ' . $file_name . ' ' . $file_type;
+			}
 
-				if (!in_array($file_ext, $extensions)) {
-					$errors[] = 'Extension not allowed: ' . $file_name . ' ' . $file_type;
-				}
+			if ($file_size > 2097152) {
+				$errors[] = 'File size exceeds limit: ' . $file_name . ' ' . $file_type;
+			}
 
-				if ($file_size > 2097152) {
-					$errors[] = 'File size exceeds limit: ' . $file_name . ' ' . $file_type;
-				}
+			if (empty($errors)) {
+				move_uploaded_file($file_tmp, $file);
+				try {
+					$conn = new PDO("mysql:host=g3v9lgqa8h5nq05o.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=r003w8gzd91dlly1", "nf7cnfvkejjel4pb", "dq9i9x5nxkgebkup");
+					} catch (PDOException $e){
+					  echo "Error ".$e->getMessage();
+					}
 
-				if (empty($errors)) {
-					move_uploaded_file($file_tmp, $file);
-					try {
-						  $conn = new PDO("mysql:host=uoa25ublaow4obx5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=ert3gsc64nwl3q5y", "ierrwzs0m1ehbuxk", "i66m2pr4nhu981fl");
-						} catch (PDOException $e){
-						  echo "Error ".$e->getMessage();
-						}
+					$avatar = $file;
+					$username = $_POST['username'];
 
-						$avatar = $file;
-						$username = $_POST['username'];
-
-						$query = "UPDATE users SET avatar='$avatar' WHERE username='$username'";
-
-						$result = $conn->query($query);
-						if($result){
-						  $users = $result->fetchAll();
-						  echo json_encode($users);
-						} else {
-						  echo json_encode(false);
-						}
-				}
+					$query = "UPDATE users SET avatar='$avatar' WHERE username='$username'";
+					
+					$result = $conn->query($query);
+					if($result){
+					  echo $avatar;
+					} else {
+					  echo json_encode(false);
+					}
 			}
 
 			if ($errors) print_r($errors);
