d3d178ced75ea3ae9eb880076e5430e9df15a3d8
Hoa Dang <hoadv106@gmail.com>
2018-12-06 15:28:06 +0700

    add user history

---
+++ getuserhistories.php
@@ -0,0 +1,20 @@
+<?php
+try {
+  $conn = new PDO("mysql:host=uoa25ublaow4obx5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=ert3gsc64nwl3q5y", "ierrwzs0m1ehbuxk", "i66m2pr4nhu981fl");
+} catch (PDOException $e){
+  echo "Error ".$e->getMessage();
+}
+
+$username = $_POST['username'];
+
+$query = "SELECT * FROM histories WHERE username='$username'";
+
+$result = $conn->query($query);
+if($result){
+  $histories = $result->fetchAll();
+  echo json_encode($histories);
+} else {
+  echo json_encode(false);
+}
+
+?>
